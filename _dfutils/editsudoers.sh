995be863d36ae1a98994723d0267797f4de781fa
B̅ <20594300+cmplstofB@users.noreply.github.com>
2020-08-17 16:00:48 +0900

    用者を外部指定/1d07c27 でも対処

--- _dfutils/editsudoers.sh
+++ _dfutils/editsudoers.sh
@@ -18,6 +18,6 @@ export PATH="$(command -p getconf PATH)${PATH:+:$PATH}"
 export POSIXLY_CORRECT='200809' UNIX_STD='2003' \
        COMMAND_MODE='unix2003' XPG_SUS_ENV='ON'
 
-<<. cat > "/etc/sudoers.d/$1"
-$(id -u -n)	ALL=(ALL) NOPASSWD: ALL
+<<. cat > "/etc/sudoers.d/50_$1"
+$1	ALL=(ALL) NOPASSWD: ALL
 .
